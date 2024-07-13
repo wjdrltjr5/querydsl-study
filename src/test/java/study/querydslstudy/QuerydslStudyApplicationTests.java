c37580b70936e42a40a6615b8b80788275684476
Jeong gi seok <wjdrltjr5@naver.com>
2024-07-13 12:17:14 +0900

    feat : entity create

--- src/test/java/study/querydslstudy/QuerydslStudyApplicationTests.java
+++ src/test/java/study/querydslstudy/QuerydslStudyApplicationTests.java
@@ -1,13 +1,33 @@
 package study.querydslstudy;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import study.querydslstudy.entity.Hello;
+import study.querydslstudy.entity.QHello;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class QuerydslStudyApplicationTests {
+
+    @Autowired
+    EntityManager em;
 
     @Test
     void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+        Hello result = query.selectFrom(qHello).fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
---
+++ src/test/java/study/querydslstudy/entity/MemberTest.java
@@ -0,0 +1,46 @@
+package study.querydslstudy.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+
+    void testEntity() {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+
+        // when
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        em.flush();
+        em.close();
+
+        List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
+        // then
+        Assertions.assertThat(members).hasSize(4);
+    }
+}
