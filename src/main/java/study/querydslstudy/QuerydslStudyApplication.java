3f29b1c20b51797fe57b874205c8f40b2b5062d8
Jeong gi seok <wjdrltjr5@naver.com>
2024-07-13 11:17:51 +0900

    init : 프로젝트 시작

---
+++ src/main/java/study/querydslstudy/QuerydslStudyApplication.java
@@ -0,0 +1,13 @@
+package study.querydslstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/study/querydslstudy/QuerydslStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.querydslstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
